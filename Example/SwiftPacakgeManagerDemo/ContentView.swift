24f59baebafd6141bfbe1d921d1dff1f5296a5cc
WataruSuzuki <wataru0406@hotmail.co.jp>
2020-12-16 22:10:00 +0900

    Added another example app

---
+++ Example/SwiftPacakgeManagerDemo/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftPacakgeManagerDemo
+//
+//  Created by watsuzuk on 2021/05/14.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example/SwiftPacakgeManagerDemo/SwiftPacakgeManagerDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftPacakgeManagerDemoApp.swift
+//  SwiftPacakgeManagerDemo
+//
+//  Created by watsuzuk on 2021/05/14.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPacakgeManagerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
